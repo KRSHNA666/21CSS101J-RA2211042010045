146ebfce900ccb8d4c067476fdcaf89f33826d41
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2022-10-06 12:03:31 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,25 @@
+
+TASK:2-ADDITION OF TWO NUMBERS:
+
+INPUT:
+
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
+
+OUTPUT:
+
+Enter two values: 10 , 11
+
+sum=21
