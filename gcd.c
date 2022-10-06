6fa4321b6bfd6c468c283b6ca0df21ed241be3b8
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2022-10-06 16:09:39 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,29 @@
+TASK:24-C PROGRAM TO FIND THE GCD OF TWO NUMBERS:
+
+INPUT:
+
+#include <stdio.h>
+int main()
+{
+    int n1, n2, i, gcd;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)
+    {
+        // Checks if i is factor of both integers
+        if(n1%i==0 && n2%i==0)
+            gcd = i;
+    }
+
+    printf("G.C.D of %d and %d is %d", n1, n2, gcd);
+
+    return 0;
+}
+
+OUTPUT:
+
+Enter two positive integers: 81
+153
+GCD = 9
