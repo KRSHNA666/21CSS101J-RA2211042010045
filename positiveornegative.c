221ea972dd5dc2293c0e339215f24719dc760e33
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2022-10-06 15:54:26 +0530

    Create positiveornegative.c

---
+++ positiveornegative.c
@@ -0,0 +1,27 @@
+TASK:19-C PROGRAM TO CHECK WHETHER A NUMBER IS POSITIVE OR NEGATIVE:
+
+INPUT:
+
+#include <stdio.h>
+
+int main() {
+
+    double num;
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+    if (num <= 0.0) {
+        if (num == 0.0)
+            printf("You entered 0.");
+        else
+            printf("You entered a negative number.");
+    } 
+    else
+        printf("You entered a positive number.");
+
+    return 0;
+}
+
+OUTPUT:
+
+Enter a number: 12.3
+You entered a positive number.
