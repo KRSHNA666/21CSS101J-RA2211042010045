b9e51b729af499081e6e6eafce82824b396a5f99
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2022-10-06 12:06:15 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,20 @@
+
+TASK:4-CALCULATING SIMPLE INTREST:
+
+
+INPUT;
+
+
+#include<stdio.h>  
+int main()   
+    {   
+        float P , R , T , SI ;  
+       P =34000; R =30;  T = 5;  
+        SI  = (P*R*T)/100;   
+        printf("\n\n Simple Interest is : %f", SI);  
+        return (0);  
+    }  
+
+OUTPUT:
+
+SI=51000.00
