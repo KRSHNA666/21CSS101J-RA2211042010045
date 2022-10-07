c6414622b12c5d77a3c2861d3d24f4703fa6696a
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2022-10-07 13:14:04 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,34 @@
+TASK:32-C Program to Check Armstrong Number:
+
+INPUT:
+
+#include <stdio.h>
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+       // remainder contains the last digit
+        remainder = originalNum % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the orignal number
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
+
+
+OUTPUT:
+
+Enter a three-digit integer: 371
+371 is an Armstrong number.
